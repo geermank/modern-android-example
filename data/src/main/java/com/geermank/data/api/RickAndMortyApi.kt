5b0bea6c92e1b7b852af7a095d12ccc2888f42bf
German Krajcar <german.krajcar@mercadolibre.com>
2021-02-27 19:49:57 -0300

    Extract string to const

--- data/src/main/java/com/geermank/data/api/RickAndMortyApi.kt
+++ data/src/main/java/com/geermank/data/api/RickAndMortyApi.kt
@@ -1,14 +1,18 @@
 package com.geermank.data.api
 
+import com.geermank.data.api.models.ResponseDto
+import com.geermank.data.models.EpisodeDto
 import javax.inject.Inject
+
+private const val API_URL = "https://rickandmortyapi.com/api/"
 
 class RickAndMortyApi @Inject constructor() : BaseApi() {
 
     private val service: RickAndMortyService = buildService()
 
-    suspend fun getAllEpisodes() = service.getAllEpisodes()
+    suspend fun getAllEpisodes(): ResponseDto<EpisodeDto> = service.getAllEpisodes()
 
     override fun getBaseUrl(): String {
-        return "https://rickandmortyapi.com/api/"
+        return API_URL
     }
 }
